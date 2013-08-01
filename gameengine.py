b1cab91d906b75121310d20fc46a7b8b4731aec7
Andrey Matveyakin <a.matveyakin@gmail.com>
2013-08-01 16:52:55 +0400

    Start writing a game not so boring as the first one

--- gameengine.py
+++ gameengine.py
@@ -1,39 +1,89 @@
+# Legend:
+# dir = "N", "S", "W", "E"
+# object = "castle", "farm", "barracks", "warrior"
+
+# Server -> client protocol:
+#  init:
+#   SizeX(int) SizeY(int) nPlayers(int) iPlayer(int)
+#   + 0th turn
+
+#  turn:
+#   x(int) y(int) owner(int) object(object) hitpoints(int)
+#   ...
+#   x(int) y(int) owner(int) object(object) hitpoints(int)
+#   end
+
+# Client -> server protocol:
+#  move  x(int) y(int) dir(dir)
+#  spawn x(int) y(int) dir(dir)
+#  build x(int) y(int) object(object)
+
+
 import playerstate as PlayerState
-from random import randint
 import functools
 
 
 class Game:
+    SizeX = 100
+    SizeY = 50
+    class Object:
+        VisionRange = 3
+        CanBeBuilt = True
+        def __init__(self):
+            self.hitpoints = self.MaxHitpoints
+
+    class Building (Object):
+        CanMove = False
+        CanAttack = False
+        pass
+    class Castle (Building):
+        MaxHitpoints = 10000
+        CanBeBuilt = False
+    class Farm (Building):
+        MaxHitpoints = 1000
+        Cost = 1000
+    class Barracks (Building):
+        MaxHitpoints = 1500
+        Cost = 1500
+
+    class Unit (Object):
+        CanMove = True
+        CanAttack = True
+        pass
+    class Warrior (Unit):
+        MaxHitpoints = 500
+        DamageToBuilding = 100
+        DamageToUnits = 100
+        Cost = 500
+
+    class Player:
+        InitialMoney = 3000
+        def __init__(self):
+            self.money = InitialMoney
+
     def __init__(self):
-        self.number = 0
-        self.increment = 42
-        self.turn = 0
-    def setPlayers(self, players):
-        self.players = players
+        pass
+    def setClients(self, clients):
+        nPlayers = len(clients)
+        assert(self.isPlayerNumAcceptable(nPlayers))
+        self.clients = clients
+        self.field = []
+        for y in range(self.SizeY):
+            field.append([None] * self.SizeX)
+        field[0][0] = Castle()
+        field[0][0].owner = 0
+        field[SizeY - 1][SizeX - 1] = Castle()
+        field[SizeY - 1][SizeX - 1].owner = 1
+        if nPlayers == 4:
+            field[0][SizeX - 1] = Castle()
+            field[0][SizeX - 1].owner = 2
+            field[SizeY - 1][0] = Castle()
+            field[SizeY - 1][0].owner = 3
+
     def processTurn(self, playerMoves):
-        self.number += self.increment
-        self.increment = randint(-10, 10)
-        #return map(lambda x: (PlayerState.LOST if not PlayerState.isFinal(x.state) else x.state, ""), self.players)
-        res = []
-        for (player, move) in zip(self.players, playerMoves):
-            if PlayerState.isFinal(player.state):
-                res.append((player.state, ""))
-                continue
-            x = None
-            try:
-                x = int(move.strip())
-            except:
-                res.append((PlayerState.LOST, "You've lost! A number expected.\n"))
-                continue
-            if x != self.number:
-                res.append((PlayerState.LOST, "You've lost! Can you add numbers?\n"))
-                continue
-            res.append((PlayerState.THINKING, str(self.increment) + "\n"))
-        if functools.reduce(lambda nInPlay, resItem: nInPlay + int(PlayerState.inPlay (resItem[0])), res, 0) == 1:
-            for player in self.players:
-                if PlayerState.inPlay(res[player.iPlayer][0]):
-                    res[player.iPlayer] = (PlayerState.WON, res[player.iPlayer][1])
-        self.turn += 1
-        return res
+        pass
     def initialMessages(self):
-        return [str(self.increment) + "\n"] * len(self.players)
+        #json.dumps({key: val for (key, val) in dict(A.__dict__).items( ) if key[:2]!='__'})  # TODO
+        pass
+    def isPlayerNumAcceptable(self, nPlayers):
+        return nPlayers in [2, 4]
