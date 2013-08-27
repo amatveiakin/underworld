d87cb69a9fe8fc9daf64c011ac433fd58df074d3
Konstantin <submultiplicativity@gmail.com>
2013-08-28 02:52:28 +0400

    [CLNUP][DOC] Add docstrings to unit building sys

--- gameengine/building.py
+++ gameengine/building.py
@@ -1,14 +1,10 @@
 # partial Game class
 from .celldefs import Cell, ObjTypes, ObjTypeDict, Building, Object
 
-def log_function(f):
-    ''' A decorator to log general function calls '''
-    def decorator(*args, **kwargs):
-        print(f.__name__, " ", args, ", ", kwargs)
-        return f(*args, **kwargs)
-    return decorator
-
-def _canBeBuilt(self, charRepr, iPlayer, asBuilding):
+def _canBeBuilt(self, charRepr, asBuilding):
+    '''
+        Can the object be built (asBuilding = true) / spawned (asBuilding = false)
+    '''
     if not charRepr in ObjTypes:
         objType = ObjTypeDict[charRepr]
     if not objType.CanBeBuilt:
@@ -16,15 +12,22 @@ def _canBeBuilt(self, charRepr, iPlayer, asBuilding):
     return asBuilding == issubclass(objType, Building)
 
 def _setBuildRequest(self, x, y, charRepr, iPlayer, asBuilding):
+    '''
+        Sets a request to build `charRep` object @(x,y) for
+        player iPlayer. if asBuilding == False, then request spawn
+    '''
     if self.field[y][x] == None: 
         self.field[y][x] = Cell( )
-    if not self._canBeBuilt(charRepr, iPlayer, asBuilding):
+    if not self._canBeBuilt(charRepr, asBuilding):
         raise Exception( ) 
     if not (x,y) in self._buildRequests:
         self._buildRequests[(x,y)] = set( )
     self._buildRequests[(x,y)].add((iPlayer, ObjTypeDict[charRepr]))
 
 def _resolveBuilding(self):
+    '''
+        Do actual building / spawning
+    '''
     for ((x,y), candidatesSet) in self._buildRequests.items( ):
         if len(candidatesSet) == 1:
             (iPlayer, objType) = candidatesSet.pop( )
