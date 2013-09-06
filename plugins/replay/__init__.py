fee69491ed1edd869b4331c17b6709ae7e3fe419
Konstantin <submultiplicativity@gmail.com>
2013-09-07 03:07:00 +0400

    [FEAT] add the forgotten file for replay plugin

---
+++ plugins/replay/__init__.py
@@ -0,0 +1,47 @@
+import gameengine
+import argparse
+import json
+import sys
+
+class ArgumentParser(argparse.ArgumentParser):
+    def error(self, message):
+        self.print_usage( )
+        sys.stderr.write("replay plugin error: " + message + "\n")
+        sys.exit(2)
+
+class Plugin:
+    def __init__(self, game, args):
+        parser = ArgumentParser(prog="")
+        parser.add_argument("--output-file", "-o", default="replay.txt")
+        parser.add_argument("--compress", "-c", default=False, action="store_true")
+        self._game = game
+        self._options = parser.parse_args(args.split( ))
+        if self._options.compress:
+            import gzip
+            import io
+            self.outputFile = io.TextIOWrapper(gzip.GzipFile(filename=self._options.output_file, mode="w"))
+        else:
+            self.outputFile = open(self._options.output_file, "w")
+        self.outputFile.write('{ "turns": [\n')
+        game.onTurnEnd = self._onTurnEnd
+    def _onTurnEnd(self):
+        d = {
+            "turn": self._game.turn,
+            "money": [p.money for p in self._game.clients],
+            "state": [str(p.state) for p in self._game.clients],
+            "objects": list(map(Plugin._getObjectDict, self._game.objects))
+        }
+        if (self._game.turn > 1):
+            self.outputFile.write(",")
+            self.outputFile.write("\n")
+        json.dump(d, self.outputFile)
+    def _getObjectDict(o):
+        d = {"x": o.x, "y": o.y, "owner": o.owner, "type": o.CharRepr}
+        if isinstance(o, gameengine.Game.ObjectWithHitpoints):
+            d["hitpoints"] = o.hitpoints
+        return d
+    def __enter__(self):
+        pass
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.outputFile.write("\n] }\n")
+        self.outputFile.close( )
