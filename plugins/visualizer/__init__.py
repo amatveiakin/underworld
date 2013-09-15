b6d40eb06eb09dfff9a4c79a84d3fac6c4b0e77d
Andrey Matveyakin <a.matveyakin@gmail.com>
2013-09-15 23:10:04 +0400

    Rework visualizer
    1. Small graphical adjustments (e.g. uniformly align letters for all units:
    both destructible and indestructible)
    2. Prepare for adding scrolling and zooming

--- plugins/visualizer/__init__.py
+++ plugins/visualizer/__init__.py
@@ -6,51 +6,51 @@ import time
 import sys
 import argparse
 
-def makeRect(x, y, cellSize):
-    return QtCore.QRect(x * cellSize, y * cellSize, cellSize, cellSize)
-ObjHPAlignMode = QtCore.Qt.AlignHCenter | QtCore.Qt.AlignBottom
-ObjAlignMode = QtCore.Qt.AlignCenter
+cellSize = 64
+hBarBorder = 6
+hBarHeight = 6
 
 class VisualizerWidget(QtGui.QWidget):
     PlayerColors = [ QtGui.QColor(255, 0, 0), QtGui.QColor(0, 0, 255),
                      QtGui.QColor(0, 255, 0), QtGui.QColor(0, 0, 0) ]
+    NeutralColor = QtGui.QColor(0, 0, 0)
     def __init__(self, parent, visualizer):
         super(VisualizerWidget, self).__init__(parent)
         self._visualizer = visualizer
+        self.transform = QtGui.QTransform()
     def paintEvent(self, event):
-        size = self.size()
-        cellWidth = size.width() / self._visualizer.game.SizeX
-        cellHeight = size.height() / self._visualizer.game.SizeY
-        cellSize = min(cellWidth, cellHeight)
-        hBarWidth = cellSize * 0.75
-        hBarHeight = cellSize * 0.1
-        hBarLeftOffset = (cellSize - hBarWidth) * 0.5
-        hBarTopOffset = cellSize * 0.1
         objects = self._visualizer.objects
-
         painter = QtGui.QPainter()
         painter.begin(self)
+        # TODO: instead allow user to scroll and zoom
+        self.transform = QtGui.QTransform()
+        size = self.size()
+        maxCellWidth = size.width() / self._visualizer.game.SizeX
+        maxCellHeight = size.height() / self._visualizer.game.SizeY
+        scaleFactor = min(maxCellWidth, maxCellHeight) / cellSize
+        self.transform.scale(scaleFactor, scaleFactor)
+        painter.setTransform(self.transform)
         painter.setFont(QtGui.QFont('', cellSize / 2))
         for o in objects:
             if o.owner >= 0:
                 mainColor = self.PlayerColors[o.owner]
             else:
-                mainColor = QtGui.QColor([0,0,0])
+                mainColor = self.NeutralColor
+            cellRect = QtCore.QRect(o.x * cellSize, o.y * cellSize, cellSize, cellSize)
+            hBarRect = cellRect.adjusted(hBarBorder, hBarBorder, -hBarBorder, -hBarBorder)
+            hBarRect.setHeight(hBarHeight)
+            textRect = cellRect
+            textRect.setTop(hBarRect.bottom())
             if isinstance(o, gameengine.Game.ObjectWithHitpoints):
-                hBarGreen = hBarWidth * o.hitpoints / o.MaxHitpoints
-                hBarRed = hBarWidth - hBarGreen 
-                hBarLeft = o.x * cellSize + hBarLeftOffset
-                hBarTop = o.y * cellSize + hBarTopOffset
+                hBarGreenRect = copy.copy(hBarRect)
+                hBarGreenRect.setWidth(hBarRect.width() * o.hitpoints / o.MaxHitpoints)
                 painter.setPen(QtGui.QColor(QtCore.Qt.black))
-                painter.setBrush(QtGui.QBrush(QtGui.QColor(QtCore.Qt.green)))
-                painter.drawRect( QtCore.QRect(hBarLeft, hBarTop, hBarGreen, hBarHeight))
                 painter.setBrush(QtGui.QBrush(QtGui.QColor(QtCore.Qt.red)))
-                painter.drawRect( QtCore.QRect(hBarLeft + hBarGreen, hBarTop, hBarRed, hBarHeight))
-                alignMode = ObjHPAlignMode
-            else:
-                alignMode = ObjAlignMode
+                painter.drawRect(hBarRect)
+                painter.setBrush(QtGui.QBrush(QtGui.QColor(QtCore.Qt.green)))
+                painter.drawRect(hBarGreenRect)
             painter.setPen(self.PlayerColors[o.owner])
-            painter.drawText( makeRect(o.x, o.y, cellSize), alignMode, o.CharRepr)
+            painter.drawText(textRect, QtCore.Qt.AlignCenter, o.CharRepr)
         painter.end()
 
 class MainWidget(QtGui.QWidget):
